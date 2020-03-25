a639bb95a818db07869b9152d10a7745fb1bc866
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2020-03-25 16:17:50 +0530

    Add files via upload

---
+++ WpfControlExample1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfControlExample
+{
+    /// <summary>
+    /// Interaction logic for WpfControlExample.xaml
+    /// </summary>
+    public partial class WpfControlExample1 : Window
+    {
+        public WpfControlExample1()
+        {
+            InitializeComponent();
+            tb.TextWrapping = TextWrapping.Wrap;
+            tb.Margin = new Thickness(10);
+            tb.Inlines.Add("An example on ");
+            tb.Inlines.Add(new Run("the TextBlock control ") { FontWeight = FontWeights.Bold });
+            tb.Inlines.Add("using ");
+            tb.Inlines.Add(new Run("inline ") { FontStyle = FontStyles.Italic });
+            tb.Inlines.Add(new Run("text formatting ") { Foreground = Brushes.Blue });
+            tb.Inlines.Add("from ");
+            tb.Inlines.Add(new Run("Code-Behind") { TextDecorations = TextDecorations.Underline });
+            tb.Inlines.Add(".");
+
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+
+        private void btnclick_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock tb = new TextBlock();
+            tb.TextWrapping = TextWrapping.Wrap;
+            tb.Margin = new Thickness(10);
+            tb.Inlines.Add("An example on ");
+            tb.Inlines.Add(new Run("the TextBlock control ") { FontWeight = FontWeights.Bold });
+            tb.Inlines.Add("using ");
+            tb.Inlines.Add(new Run("inline ") { FontStyle = FontStyles.Italic });
+            tb.Inlines.Add(new Run("text formatting ") { Foreground = Brushes.Blue });
+            tb.Inlines.Add("from ");
+            tb.Inlines.Add(new Run("Code-Behind") { TextDecorations = TextDecorations.Underline });
+            tb.Inlines.Add(".");
+           // this.st.Content = tb;
+            st.Children.Add(tb);
+
+        }
+
+    }
+}
